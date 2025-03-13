OCA/timesheet

[ADD] sale_timesheet_analytic_propagation: update timesheet analytic account from task.
This module allows to update the timesheet's analytic account when the task related to
it changes the analytic account. This only happens if the timesheet is not invoiced yet.

@moduon MT-5142

sale_timesheet_analytic_propagation/__manifest__.py
@@ -1,0 +1,19 @@
+# Copyright 2024 Moduon Team S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/LGPL-3.0)
+
+{
+    "name": "Sale Timesheet Analytic Propagation",
+    "summary": "Updates timesheet's analytic account when their task changes the analytic.",
+    "version": "15.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Timesheet",
+    "website": "https://github.com/OCA/timesheet",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["edlopen", "rafaelbn"],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_timesheet",
+    ],
+}

c4a8-odoo/module-oca-timesheet

project_task_analytic_propagation/__manifest__.py
@@ -1,0 +1,19 @@
+# Copyright 2024 Moduon Team S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/LGPL-3.0)
+
+{
+    "name": "Project Task Analytic Propagation",
+    "summary": "Updates timesheet's analytic account when their task changes the analytic.",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Timesheet",
+    "website": "https://github.com/OCA/timesheet",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["edlopen", "rafaelbn"],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_timesheet",
+    ],
+}