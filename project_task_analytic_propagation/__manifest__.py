8c90d3be82dc6108691b0f2f8683fbd24ef29336
Andrii Kompaniiets <andrii@moduon.team>
2025-07-21 13:56:11 +0200

    [MIG] project_task_analytic_propagation: Migration to 18.0

--- project_task_analytic_propagation/__manifest__.py
+++ project_task_analytic_propagation/__manifest__.py
@@ -5,16 +5,19 @@
     "name": "Project Task Analytic Propagation",
     "summary": "Updates timesheet's analytic account"
     " when their task changes the analytic.",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.1",
     "development_status": "Alpha",
     "category": "Timesheet",
     "website": "https://github.com/OCA/timesheet",
     "author": "Moduon, Odoo Community Association (OCA)",
-    "maintainers": ["edlopen", "rafaelbn"],
+    "maintainers": ["Andrii9090", "rafaelbn"],
     "license": "LGPL-3",
     "application": False,
     "installable": True,
     "depends": [
         "sale_timesheet",
+    ],
+    "data": [
+        "views/project_task_views.xml",
     ],
 }
